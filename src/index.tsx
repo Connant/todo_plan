b7dd359aee63def6d63cfdc6c0255f1350be67a5
Дарья 🥝 <daria@MacBook-Pro-Dara.local>
2022-09-22 15:09:59 +0700

    added persistor

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.scss";
 import App from "./components/App";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { persistor, store } from "./store/store";
 
 const element = document.getElementById("root")!;
-
 
 const root = ReactDOM.createRoot(element);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import thunk from 'redux-thunk';
 import appDataReducer from "./reducer";
 
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, appDataReducer)
+
 
 export const store = configureStore({
-  reducer: appDataReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware()
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk]
 });
 
+export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
